e5e1d951ffccbddc30492c17600fb7e178cf0d39
sanchjain <54800651+sanchjain@users.noreply.github.com>
2020-04-19 11:23:00 +0530

    Update settings.py

--- couponsite/settings.py
+++ couponsite/settings.py
@@ -82,10 +82,10 @@ DATABASES = {
     }
 }
 
-import dj_database_url
+# import dj_database_url
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 
 # Password validation
